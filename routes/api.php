a2cbe642750fc73705a4ce8e8620294b6b13f193
Arthur Pavezzi <arthur.henrique.pavezzi@gmail.com>
2020-11-19 04:48:10 -0300

    Redirecting API routes to ApiController

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('airports', [Controllers\ApiController::class, 'getAllAirports']);
+
+Route::get('airports/{id}', [Controllers\ApiController::class, 'getSingleAirport']);
+
+$otherVerbs = ['delete', 'options', 'patch', 'post', 'put', 'view'];
+Route::match($otherVerbs, 'airports', [Controllers\ApiController::class, 'handleOtherVerbs']);
+
+Route::match($otherVerbs, 'airports/{id}', [Controllers\ApiController::class, 'handleOtherVerbs']);
